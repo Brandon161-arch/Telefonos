491e240c403987382aa3a2b2815b8cfbb2e97db1
Brandon161-arch <brandonjoseduarte823@gmail.com>
2026-04-15 23:31:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
+
+    telefono= [ {"nombre": "iPhone 13", "precio": 3500000, "imagen": "iphone.jpg"},
+        {"nombre": "Samsung S23", "precio": 3200000, "imagen": "samsung.jpg"},
+        {"nombre": "Xiaomi Redmi Note 12", "precio": 1200000, "imagen": "xiaomi.jpg"}]
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
+
