c2f10fa6b18ee282cdd28acb136effdfbc56f3fa
Brandon161-arch <brandonjoseduarte823@gmail.com>
2026-04-17 23:59:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def inicio():
 
     telefono= [ {"nombre": "iPhone 13", "precio": 3500000, "imagen": "iphone.jpg"},
-        {"nombre": "Samsung S23", "precio": 3200000, "imagen": "samsung.jpg"},
+        {"nombre": "Samsung S2", "precio": 3200000, "imagen": "samsung.jpg"},
         {"nombre": "Xiaomi Redmi Note 12", "precio": 1200000, "imagen": "xiaomi.jpg"}]
     return render_template("index.html")
 
